1311ac136d4896f8c00ecc3843ef7effdba9bdbc
Irmene-Valerie Leonard <ileonard@uwo.ca>
2022-06-04 15:22:03 -0400

    files for GeoJSON lineStrings

--- Mapping_GeoJSON_LineStrings/static/js/logic.js
+++ Mapping_GeoJSON_LineStrings/static/js/logic.js
@@ -41,16 +41,10 @@ d3.json(torontoData).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
 L.geoJSON(data,{
-  color: "yellow",
-  weight: 2
-}).addTo(map);
-
-L.geoJSON(data, {
+  color: "#ffffa1",
+  weight: 2,
   onEachFeature: function(feature, layer) {
-    console.log(layer);
     layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr> <h3> Destination: " + feature.properties.dst + "</h3>");
    }
 }).addTo(map);
-
-
 });
